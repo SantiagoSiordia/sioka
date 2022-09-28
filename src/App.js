f8d0e76b720027eebed2abe374f83e1437173ece
Santiago Siordia <santiago.gonzalez@wizeline.com>
2022-09-28 10:54:58 -0500

    (:

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ImgCarousel from './components/carousel/ImgCarousel';
+import Destinations from './components/destinations/Destinations'
+import Footer from './components/footer/Footer';
+import Hero from './components/hero/Hero'
+import Navbar from './components/navbar/Navbar'
+import Search from './components/search/Search';
+import Selects from './components/selects/Selects';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Destinations />
+      <Search />
+      <Selects />
+      <ImgCarousel />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
