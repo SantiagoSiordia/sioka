40a9c1dd3b3e2a7f769b215680783e5061137a6d
Santiago Siordia <santiago.gonzalez@wizeline.com>
2022-09-28 17:46:44 -0500

    fixed carousel

--- src/components/carousel/ImgCarousel.js
+++ src/components/carousel/ImgCarousel.js
@@ -8,26 +8,44 @@ import Cancun from '../../assets/cancun.jpeg';
 import Chiapas from '../../assets/chiapas.webp';
 import IslaMujeres from '../../assets/isla-mujeres.png';
 
+const renderIndicator = (onClickHandler, isSelected, index, label) => {
+    const defStyle = { marginLeft: "1rem",borderRadius: 50, height: "1rem", width: "1rem", cursor: "pointer" };
+    const style = isSelected
+    ? { ...defStyle, backgroundColor: "#709dff" }
+    : { ...defStyle, backgroundColor: "#555" };
+
+    return (
+        <div
+            style={style}
+            onClick={onClickHandler}
+            onKeyDown={onClickHandler}
+            value={index}
+            key={index}
+            role="button"
+            tabIndex={0}
+            aria-label={`${label} ${index + 1}`}
+        />
+    );
+}
+
 function ImgCarousel() {
     return (
         <div name='carousel' className='container'>
-            <Carousel className='carousel' showArrows={true} autoPlay={false} infiniteLoop={true} >
+            <Carousel renderIndicator={renderIndicator} className='carousel' showArrows={false} infiniteLoop swipeable >
                 <div>
                     <img src={Chiapas} alt='/' />
-                    {/* <p className="legend">Maldives 1</p> */}
+                    {/* <p className="legend">Chiapas</p> */}
                 </div>
                 <div>
                     <img src={IslaMujeres} alt='/' />
-                    {/* <p className="legend">Legend 2</p> */}
+                    {/* <p className="legend">IslaMujeres</p> */}
                 </div>
                 <div>
                     <img src={Cancun} alt='/' />
-                    {/* <p className="legend">Legend 3</p> */}
+                    {/* <p className="legend">Cancun</p> */}
                 </div>
             </Carousel>
         </div>
-
-
     )
 }
 
