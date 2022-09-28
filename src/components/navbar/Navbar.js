b322181805f68a3278e506fde6d039142d199e47
Santiago Siordia <santiago.gonzalez@wizeline.com>
2022-09-28 18:32:24 -0500

    removed nav icons

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react'
 import { BrowserView, MobileView } from 'react-device-detect'
 import { AiOutlineClose } from 'react-icons/ai'
-import { BiSearch } from 'react-icons/bi'
-import { BsPerson } from 'react-icons/bs'
 import { FaFacebook, FaInstagram, FaPinterest, FaTwitter, FaYoutube } from 'react-icons/fa'
 import { HiOutlineMenuAlt4 } from 'react-icons/hi'
 import { Link } from 'react-scroll'
@@ -27,10 +25,6 @@ function Navbar() {
                 <Link to='search' smooth={true} duration={500} ><li>Book</li></Link>
                 <Link to='views' smooth={true} duration={500} ><li>Views</li></Link>
             </ul>
-            <div className="nav-icons">
-                <BiSearch className='icon' style={{ marginRight: '1rem' }} />
-                <BsPerson className='icon' />
-            </div>
             <div className="hamburger" onClick={handleNav}>
                 {!nav ? (<HiOutlineMenuAlt4 className='icon' />) : (<AiOutlineClose style={{ color: '#000' }} className='icon' />)}
 
